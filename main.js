cd0a13ac02942fbc04b76c0dec1a262d756b8585
2Changele2 <mariad.a.hidalgo@gmail.com>
2022-06-14 21:23:50 +0200

    Pruebas

--- main.js
+++ main.js
@@ -1,37 +1,107 @@
-var path = require('path');
+const path = require('path');
 const express = require('express');
+const fs = require("fs")
+const Mustache = require('mustache');
+const mustacheExpress = require("mustache-express")
 const app = express();
 const PORT = process.env.PORT;
-const STATIC_FOLDER = path.join(__dirname, '..', 'tiempo_cliente');
+const STATIC_PATH = path.join(__dirname, '..', 'tiempo_cliente');
+const VIEWS_PATH = path.join(__dirname, 'views');
+
+// To set functioning of mustachejs view engine
 
 app.use(express.urlencoded({
     extended: true
 }));
 
-app.post('/login_action', (req, res)=>{ 
 
-    const {name, pass} = req.body;
+
+
+// Setting mustachejs as view engine
+// app.set('views',path.join(__dirname,'views'));
+// app.set('view engine','html');
+
+// Register '.mustache' extension with The Mustache Express
+app.engine('mustache', mustacheExpress());
+
+app.set('view engine', 'mustache');
+app.set('views', path.join(__dirname,'views'));
+
+app.engine('mst', mustacheExpress(VIEWS_PATH, '.mst'));
+
+
+
+
+
+
+
+
+
+
+//rendering example for response
+
+
+app.get('/ejemplo',(req,res)=>{
+    res.render('ejemplo.mst',{msg:'tu pyagdhgj'});
+});
+
+
+app.get('/nuevoindex',(req,res)=>{
+    console.log("aquiiiii")
+
+
+
+
+
+    /// const pre = Mustache.render(path.join(__dirname,'views','pre.mst'))
+
+
+    var head = ""
+    try 
+    {
+        const data = fs.readFileSync(path.join(__dirname,'views','head.mst'));
+        head = data
+    } catch (err) {
+        console.error(err);
+    }
+
+    console.log(head)
+
+    res.render('index.mst',{
+        head: head, 
+        ubicaciones: "<span>aaaaaa</span>"
+    });
+});
+
+
+
+
+
+
+
+app.post('/login_action', (req, res) => {
+    const {name, pass} = req.query;
+
 
     console.log(`Aqui esta el user ${name} con el pass ${pass}`);
 
     console.log("LOGIN TODO");
     res.send("LOGIN");
 })
-app.post('/registro_action', (req, res)=>{ 
-        
+
+app.post('/registro_action', (req, res) => {
+
     console.log("registro_user TODO");
     res.send("registro_user");
 })
-app.use('/yoqueseu', (req, res)=>{ 
+
+app.use('/yoqueseu', (req, res) => {
     console.log("yoqueseu TODO");
     res.send("yoqueseu");
 })
 
+app.use('/', express.static(STATIC_PATH));
 
-app.use('/', express.static(STATIC_FOLDER));
-
-
-app.listen(PORT, function(){
-    console.log(`Servidor web escuchando en el puerto ${PORT}, sirviendo el contenido de ${STATIC_FOLDER}`);
+app.listen(PORT, function() {
+    console.log(`Servidor web escuchando en el puerto ${PORT}, sirviendo el contenido de ${STATIC_PATH}`);
 });
-
