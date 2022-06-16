01b2d1ed94a05aa32ae900e018f3f7d1d1e70db7
Carlos <carmesce@gmail.com>
2022-06-16 21:00:50 +0200

    Conectado main con ddbb

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const path = require('path');
 const express = require('express');
-const fs = require("fs")
+const fs = require("fs");
 const Mustache = require('mustache');
 const mustacheExpress = require("mustache-express")
+const ddbb = require("./ddbb");
+
 const app = express();
 const PORT = process.env.PORT;
 const STATIC_PATH = path.join(__dirname, '..', 'tiempo_cliente');
 const VIEWS_PATH = path.join(__dirname, 'views');
+
+
 
 // To set functioning of mustachejs view engine
 
@@ -47,12 +51,7 @@ app.get('/ejemplo',(req,res)=>{
 app.get('/nuevoindex',(req,res)=>{
     console.log("aquiiiii")
 
-
-
-
-
     /// const pre = Mustache.render(path.join(__dirname,'views','pre.mst'))
-
 
     var head = ""
     try 
@@ -71,26 +70,22 @@ app.get('/nuevoindex',(req,res)=>{
     });
 });
 
-
-
-
-
-
-
-app.post('/login_action', (req, res) => {
-    const {name, pass} = req.query;
-
+app.use('/login_action', (req, res) => {
+    const {name, pass} = req.body;
 
     console.log(`Aqui esta el user ${name} con el pass ${pass}`);
 
-    console.log("LOGIN TODO");
-    res.send("LOGIN");
+    let newUserId = ddbb.registrarUsuario(name, pass);
+
+    res.send(`Aqui esta el user ${name} con el pass ${pass} se ha regisrado con el ID: ${newUserId}`);
 })
 
 app.post('/registro_action', (req, res) => {
 
     console.log("registro_user TODO");
     res.send("registro_user");
+
+
 })
 
 app.use('/yoqueseu', (req, res) => {
