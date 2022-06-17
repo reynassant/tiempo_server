674659df81954ae8de362dce20404c88db4e1281
Vincenzo Cancelliere <vincen85@gmail.com>
2022-06-17 22:24:31 +0200

    ESlint
    and other code style improvements

--- ddbb.js
+++ ddbb.js
@@ -1,63 +1,36 @@
-const dotenv = require('dotenv').config();
-const crypto = require('crypto');
-const mysql = require('mysql');
+const crypto = require("crypto");
+const { executeQuery } = require("./ddbb-core");
 
-const apiKey = {
-    positionStack: "4135e834e062ecba56ae7aaa12e8c353",
-    openWeatherMap: "574e6bfed3631e8850f21f8c2cf9c59d"
-}
+function registrarUsuario(usuario, pass, res) {
+  function getDatetime() {
+    var currentdate = new Date();
+    var datetime =
+      currentdate.getFullYear() +
+      "-" +
+      currentdate.getMonth() +
+      "-" +
+      currentdate.getDay() +
+      " " +
+      currentdate.getHours() +
+      ":" +
+      currentdate.getMinutes() +
+      ":" +
+      currentdate.getSeconds();
 
-const ddbbConf = {
-    host: "localhost",
-    user: "root",
-    password: process.env.SQLPASS || "krono",
-    database: "ltiempo",
-}
+    return datetime;
+  }
 
-function executeQuery(query, callback, res) {
-   
-    var con = mysql.createConnection(ddbbConf);
-    try
-    {
-        con.connect(function(err) {
-            if (err) throw err;
+  var newUser = {
+    usuario: usuario || `U_${getDatetime()}`,
+    pass: pass || "abcde",
+    salt: crypto.randomBytes(22).toString("hex"),
+  };
+  newUser.hash = crypto
+    .createHash("sha256")
+    .update(newUser.pass + newUser.salt)
+    .digest("hex");
 
-            con.query(query, function(err, result, fields) {
-                if (err) throw err;
-                con.end();
-                callback(result, res);
-            });
-        });
-    } catch (err) {
-        con.end();
-        console.log(err);
-    }
-}
-
-function registrarUsuario(usuario, pass, res)
-{
-    function getDatetime() {
-        var currentdate = new Date();
-        var datetime =
-            currentdate.getFullYear() + '-' +
-            currentdate.getMonth() + '-' +
-            currentdate.getDay() + " " +
-            currentdate.getHours() + ":" +
-            currentdate.getMinutes() + ":" +
-            currentdate.getSeconds();
-    
-        return datetime;
-    }
-
-    var newUser = 
-    {
-        usuario: usuario || `U_${getDatetime()}`,
-        pass: pass || 'abcde',
-        salt: crypto.randomBytes(22).toString("hex"),
-    }
-    newUser.hash = crypto.createHash('sha256').update(newUser.pass + newUser.salt).digest('hex');
-
-    var queryNewUser = `
+  var queryNewUser = `
         INSERT INTO registro
         (usuario, hash, salt)
         VALUES
@@ -67,72 +40,64 @@ function registrarUsuario(usuario, pass, res)
             '${newUser.salt}'
         );`;
 
-    const callbackNewUser = function(result, res) {
-        console.log(`Inserted User: ${result.insertId}`);
-        res.send(`Inserted User: ${result.insertId}`);
-    }
+  const callbackNewUser = function (result, res) {
+    console.log(`Inserted User: ${result.insertId}`);
+    res.send(`Inserted User: ${result.insertId}`);
+  };
 
-    executeQuery(queryNewUser, callbackNewUser, res);
+  executeQuery(queryNewUser, callbackNewUser, res);
 }
 
-function loginUsuario(usuario, pass, res)
-{
-    // newUser.hash = crypto.createHash('sha256').update(newUser.pass + newUser.salt).digest('hex');
-    var queryLogin = `
+function loginUsuario(usuario, pass, res) {
+  // newUser.hash = crypto.createHash('sha256').update(newUser.pass + newUser.salt).digest('hex');
+  var queryLogin = `
     SELECT idRegistro, usuario, \`hash\`, salt 
     FROM ltiempo.registro 
     WHERE usuario="${usuario}";`;
 
+  const callbackLogin = function (result, res) {
+    var resultado = undefined;
 
-    const callbackLogin = function(result, res){
-        var resultado = undefined;
-
-        if (result.length <= 0) {
-            resultado = {
-                status: 1, // 0 Usuario logueado, 1 Usuario incorrecto, 2 Contraseña incorrecta
-                idUser: undefined, 
-                nameUser: undefined,
-                msg: "Usuario incorrecto" 
-            };
-        } else { // Si existe entonces verificamos contraseña            
-            //Calculamos hash de input
-            var hash_in = crypto.createHash('sha256').update(pass + result[0].salt).digest('hex')
-            // console.log(pass_in)
-            // console.log(result[0].hash)
-            // Comparamos el hash de input con el hash de la base de datos
-            if (hash_in == result[0].hash) {
-                // console.log('Usuario Autenticado!!!! Puede Acceder a la Pagina');
-                resultado = resultado = {
-                    status: 0, // 0 Usuario logueado, 1 Usuario incorrecto, 2 Contraseña incorrecta
-                    idUser: result[0].idRegistro,
-                    nameUser: result[0].usuario,
-                    msg: "Usuario logueado" 
-                };
-            } else {
-                resultado = {
-                    status: 2, // 0 Usuario logueado, 1 Usuario incorrecto, 2 Contraseña incorrecta
-                    idUser: undefined, 
-                    nameUser: undefined,
-                    msg: "Constraseña incorrecta" 
-                }
-            }                    
-        } 
-
-        res.send(resultado);
+    if (result.length <= 0) {
+      resultado = {
+        status: 1, // 0 Usuario logueado, 1 Usuario incorrecto, 2 Contraseña incorrecta
+        idUser: undefined,
+        nameUser: undefined,
+        msg: "Usuario incorrecto",
+      };
+    } else {
+      // Si existe entonces verificamos contraseña
+      //Calculamos hash de input
+      var hash_in = crypto
+        .createHash("sha256")
+        .update(pass + result[0].salt)
+        .digest("hex");
+      // console.log(pass_in)
+      // console.log(result[0].hash)
+      // Comparamos el hash de input con el hash de la base de datos
+      if (hash_in == result[0].hash) {
+        // console.log('Usuario Autenticado!!!! Puede Acceder a la Pagina');
+        resultado = resultado = {
+          status: 0, // 0 Usuario logueado, 1 Usuario incorrecto, 2 Contraseña incorrecta
+          idUser: result[0].idRegistro,
+          nameUser: result[0].usuario,
+          msg: "Usuario logueado",
+        };
+      } else {
+        resultado = {
+          status: 2, // 0 Usuario logueado, 1 Usuario incorrecto, 2 Contraseña incorrecta
+          idUser: undefined,
+          nameUser: undefined,
+          msg: "Constraseña incorrecta",
+        };
+      }
     }
 
-    executeQuery(queryLogin, callbackLogin, res);
-}
+    res.send(resultado);
+  };
 
-/*
-{
-    status: 1, // 0 usuario logueado, 1 Usuario incorrecto, 2 Contraseña incorrecta
-    idUser: undefined, 
-    nameUser: undefined,
-    msg: "Usuario incorrecto" 
-
+  executeQuery(queryLogin, callbackLogin, res);
 }
-*/
 
 // loginUsuario("Pedro", "123");
 // registrarUsuario("usuario987987", "pass");
@@ -141,144 +106,9 @@ function loginUsuario(usuario, pass, res)
 // function addUbicacion(id_usuario, nombre, lat, lon) {}
 // function listaUbicacion(id_usuario) {}
 
-/*
-function imprimeUsuarios(result) {
-    function imprimeUsuario(u) {
-        console.log(`U_Invent: <${u.idRegistro}--${u.usuario}>>>>>>>[${u.hash}] -- (${u.salt})`);
-
-    }
-
-    for (i in result) {
-        imprimeUsuario(result[i]);
-
-    }
-}
-
-con.query("SELECT * FROM registro;", function(err, result, fields) {
-    if (err) throw err;
-    //console.log(result);
-    //imprimeUsuarios(result)
-
-});
-
-var queryInsCiudad = `INSERT INTO ciudades (ciudad,lat,lon) VALUES (?,?,?);`;
-
-var querySel = "SELECT * FROM registro;";
-
-con.query(querySel, function(err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-    imprimeUsuarios(result)
-});
-
-var UsrtInpt = {
-    usr_input: '06_usuario',
-    pass_input: 'abcde'
-};
-
-var queryFindUsr = "SELECT * FROM registro WHERE usuario = ?;";
-
-con.query(queryFindUsr, [UsrtInpt.usr_input], function(err, result, fields) {
-    if (err) throw err;
-    // Si no conseguimos el usuario
-    if (result.length <= 0) {
-        console.log('Usuario no existe')
-    } else { // Si existe entonces verificamos contraseña
-
-        console.log('Usuario Existe, verificando contrasena:')
-        var hash_in = crypto.createHash('sha256').update(UsrtInpt.pass_input + result[0].salt).digest('hex')
-        // console.log(pass_in)
-        // console.log(result[0].hash)
-        if (hash_in == result[0].hash) {
-            console.log('Usuario Autenticado!!!! Puede Acceder a la Pagina')
-        } else {
-            console.log('Password invalida, intentelo de nuevo')
-        }
-        //crypto.createHash('sha256').update(newUser.pass + newUser.salt).digest('hex')
-    }
-    // console.log('Resultado es:');
-    // console.log(result);
-
-    var InputCiudad = "Guadalajara Espana"
-    console.log("Buscando " + InputCiudad + " en la base de datos")
-    var queryBuscaCiudad = "SELECT * FROM ciudades WHERE ciudad = ?;";
-    con.query(queryBuscaCiudad, [InputCiudad], function(err, result, fields) {
-        if (err) throw err;
-
-        // Si no conseguimos la ciudad
-
-        if (result.length <= 0) {
-            console.log(InputCiudad + ' no existe en la base datos')
-
-            console.log("Buscando " + InputCiudad + " en pointstack API")
-            var configPS = {
-                method: 'get',
-                url: `http://api.positionstack.com/v1/forward?access_key=${apiKey.positionStack}&query=${InputCiudad}`,
-                headers: {}
-            };
-
-            axios(configPS)
-                .then(function(response) {
-                    //console.log(JSON.stringify(response.data));
-                    //console.log(response.data.data)
-                    if (response.data.data.length > 0) {
-                        var ciudadAPI = response.data.data[0]
-                        var ciudadOutput = ciudadAPI
-                        console.log(ciudadAPI)
-                        console.log("Ciudad Encontrada:" + ciudadAPI.label)
-
-                        // Tengo que verificar que la ciudad no esta guardada en el database con el label que  obtuve de la API
-
-                        con.query(queryBuscaCiudad, [ciudadAPI.label], function(err, result, fields) {
-                            if (err) throw err;
-                            // Si no conseguimos la ciudad
-                            if (result.length <= 0) {
-                                console.log(ciudadAPI.label + ' no existe en la base datos')
-                                console.log("Agregar " + ciudadAPI.label + " a la base de datos")
-                                con.query(queryInsCiudad, [ciudadAPI.label, ciudadAPI.latitude, ciudadAPI.longitude], function(err, result, fields) {
-                                    if (err) throw err;
-                                    console.log("Ciudad " + ciudadAPI.label + " agregada a la base de datos")
-                                });
-                            };
-                        });
-
-                        // Teniendo Latitud y Longitud buscamos el tiempo con openweather
-
-                        var configOWM = {
-                            method: 'get',
-                            url: `https://api.openweathermap.org/data/2.5/onecall?lat=${ciudadAPI.latitude}&lon=${ciudadAPI.longitude}&exclude=hourly,daily&appid=${apiKey.openWeatherMap}&units=metric&lang=ES`,
-                            headers: {}
-                        };
-
-                        axios(configOWM)
-                            .then(function(response) {
-                                //console.log(JSON.stringify(response.data));
-                                console.log('Temperatura:' + response.data.current.temp + "C")
-                                console.log('Sensacion terminca:' + response.data.current.feels_like + "C")
-                                console.log('Humedad:' + response.data.current.humidity + `%`)
-                                console.log(response.data.current.weather[0].description)
-                            })
-                            .catch(function(error) {
-                                console.log(error);
-                            });
-
-
-
-                    } else {
-                        console.log("Ciudad no encontrada")
-                    }
-                })
-                .catch(function(error) {
-                    console.log(error);
-                });
-        }
-    });
-});
-*/
-
 module.exports = {
-    registrarUsuario: registrarUsuario,
-    loginUsuario: loginUsuario,
-    // addUbicacion: addUbicacion,
-    // listaUbicacion: listaUbicacion,
-}
+  registrarUsuario: registrarUsuario,
+  loginUsuario: loginUsuario,
+  // addUbicacion: addUbicacion,
+  // listaUbicacion: listaUbicacion,
+};
--- main.js
+++ main.js
@@ -1,46 +1,47 @@
-const path = require('path');
-const express = require('express');
 const fs = require("fs");
-const Mustache = require('mustache');
-const mustacheExpress = require("mustache-express")
+const path = require("path");
+const express = require("express");
+const mustacheExpress = require("mustache-express");
 const ddbb = require("./ddbb");
 
 const app = express();
 const PORT = process.env.PORT;
-const STATIC_PATH = path.join(__dirname, '..', 'tiempo_cliente');
-const VIEWS_PATH = path.join(__dirname, 'views');
+const STATIC_PATH = path.join(__dirname, "..", "tiempo_cliente");
+const VIEWS_PATH = path.join(__dirname, "views");
 
 
 
 // To set functioning of mustachejs view engine
 
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 // Setting mustachejs as view engine
 // app.set('views',path.join(__dirname,'views'));
 // app.set('view engine','html');
 
 // Register '.mustache' extension with The Mustache Express
-app.engine('mustache', mustacheExpress());
-app.set('view engine', 'mustache');
-app.set('views', path.join(__dirname, 'views'));
-app.engine('mst', mustacheExpress(VIEWS_PATH, '.mst'));
+app.engine("mustache", mustacheExpress());
+app.set("view engine", "mustache");
+app.set("views", path.join(__dirname, "views"));
+app.engine("mst", mustacheExpress(VIEWS_PATH, ".mst"));
 
 
 //rendering example for response
 
 let getTemplate = (template) => {
-    var content = ""
-    try {
-        content = fs.readFileSync(path.join(__dirname, 'views', template), "utf8");;
-    } catch (err) {
-        console.error(err);
-    }
+  var content = "";
+  try {
+    content = fs.readFileSync(path.join(__dirname, "views", template), "utf8");
+  } catch (err) {
+    console.error(err);
+  }
 
-    return content;
-}
+  return content;
+};
 
 app.get('/nuevoindex', (req, res) => {
     /// const pre = Mustache.render(path.join(__dirname,'views','pre.mst'))
